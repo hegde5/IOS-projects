2bbeb4e2aa57002d81eaeaa3e9ce7245052783cb
Vinay Hegde <hegde.vi@dmca82066001cc5.nunet.neu.edu>
2015-06-19 15:30:15 -0400

    Initial Commit

---
+++ Man's Best Friend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Man's Best Friend
+//
+//  Created by Vinay Hegde on 6/19/15.
+//  Copyright (c) 2015 Vinay Hegde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
