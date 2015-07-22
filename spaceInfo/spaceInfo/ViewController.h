e6826c985fb050c867894f3d73d949a540ddc10c
Vinay Hegde <hegde.vi@dmc406c8f2e9caa.nunet.neu.edu>
2015-07-22 17:39:16 -0400

    Space Info project initial commit

---
+++ spaceInfo/spaceInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  spaceInfo
+//
+//  Created by Vinay Hegde on 7/22/15.
+//  Copyright (c) 2015 Vinay Hegde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ spaceInfo/spaceInfo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  spaceInfo
+//
+//  Created by Vinay Hegde on 7/22/15.
+//  Copyright (c) 2015 Vinay Hegde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
